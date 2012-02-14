d0509f2bce3c7ab33ef8a1147b68b6e88260ffc5
Bartosz Inglot <jhi@o2.pl>
2012-02-14 19:16:34 +0000

    Started refactoring Source class.

--- src/org/Zeitline/Source.java
+++ src/org/Zeitline/Source.java
@@ -7,57 +7,58 @@ import java.io.File;
 
 import org.Zeitline.Timestamp.Timestamp;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Vector;
 
 public class Source
         implements Serializable {
 
-    public static final long GRANULARITY_MILLI = 1,
-            GRANULARITY_SEC = 1000,
-            GRANULARITY_MIN = 60000;
+    public static final long GRANULARITY_MILLI = 1;
+    public static final long GRANULARITY_SEC = 1000;
+    public static final long GRANULARITY_MIN = 60000;
 
-    private Vector event_list;
+    private final List<AtomicEvent> eventList = new ArrayList<AtomicEvent>();
     private Timestamp created;
     private String name; // filter type name
 
     // none of the following variables are currently being used
     // these are for future reference and/or development only
-    private String input_source; // full path file name
+    private String inputSource; // full path file name
     private String host; // should there be a seperate Host object?
     private long granularity;
-    private int userid;
+    private int userId;
 
-    public Source(String name, String input_source, long granularity) {
+    public Source(String name, String inputSource, long granularity) {
         this.created = new Timestamp(new Date().getTime());
         this.name = name;
-        this.input_source = input_source;
+        this.inputSource = inputSource;
         this.granularity = granularity;
-        this.event_list = new Vector();
-    } // org.Zeitline.Source
+    }
 
     public void addEvent(AtomicEvent event) {
-        event_list.add(event);
+        eventList.add(event);
     }
 
     public boolean removeEvent(AtomicEvent event) {
-        return event_list.remove(event);
+        return eventList.remove(event);
     }
 
-    public Vector getEvents() {
-        return event_list;
+    public List<AtomicEvent> getEvents() {
+        return eventList;
     }
 
     public Timestamp getTimestamp() {
         return created;
-    } // getTimestamp
+    }
 
     public String getName() {
         return name;
-    } // getName
+    }
 
     public String toString() {
-        String filename = (new File(input_source)).getName();
+        String filename = (new File(inputSource)).getName();
         return name + ": " + filename;
-    } // toString
-} // class org.Zeitline.Source
+    }
+}
