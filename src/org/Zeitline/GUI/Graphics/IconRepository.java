625a303ef7f8020770d41493bef2cc4b04e076dd
Bartosz Inglot <jhi@o2.pl>
2012-02-21 18:40:05 +0000

    Deleted unnecessary lines.

--- src/org/Zeitline/GUI/Graphics/IconRepository.java
+++ src/org/Zeitline/GUI/Graphics/IconRepository.java
@@ -1,6 +1,5 @@
 package org.Zeitline.GUI.Graphics;
 
-import org.Zeitline.GUI.Action.FindAction;
 import org.Zeitline.Utils;
 import org.Zeitline.Zeitline;
 
@@ -10,21 +9,6 @@ import java.net.URL;
 public class IconRepository implements  IIconRepository<ImageIcon> {
     private static final String ICONS_DIR = "icons";
     private static final String ICONS_EXTENSION = ".png";
-
-//    saveAction = new SaveAction(this, icons.getIcon("filesave"), KeyEvent.VK_S);
-//    loadAction = new LoadAction(this, icons.getIcon("fileopen"), KeyEvent.VK_L);
-//    cutAction = new CutAction(this, icons.getIcon("editcut"), KeyEvent.VK_T);
-//    pasteAction = new PasteAction(this, icons.getIcon("editpaste"), KeyEvent.VK_P);
-//    findAction = new FindAction(this, icons.getIcon("find"), KeyEvent.VK_D);
-//    createFrom = new CreateFromAction(this, icons.getIcon("create_event"), KeyEvent.VK_C);
-//    removeEvents = new RemoveEventsAction(this, icons.getIcon("delete_event"), KeyEvent.VK_R);
-//    importAction = new ImportAction(this, icons.getIcon("import"), KeyEvent.VK_I, inputFilters);
-//    emptyTimeline = new EmptyTimelineAction(this, icons.getIcon("new_timeline"), KeyEvent.VK_E);
-//    createTimelineFrom = new CreateTimelineFromAction(this, icons.getIcon("create_timeline"), KeyEvent.VK_C);
-//    deleteTimeline = new DeleteTimelineAction(this, icons.getIcon("delete_timeline"), KeyEvent.VK_D);
-//    moveLeft = new MoveLeftAction(this, icons.getIcon("moveleft"), KeyEvent.VK_L);
-//    moveRight = new MoveRightAction(this, icons.getIcon("moveright"), KeyEvent.VK_R);
-//    filterQueryAction = new FilterQueryAction(this, icons.getIcon("filter"), KeyEvent.VK_F);
 
     public ImageIcon getIcon(IconNames name) {
         String fileName = null;
