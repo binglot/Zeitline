081252f87d7b422fb5d4aabd2b195b4df89447dc
Bartosz Inglot <jhi@o2.pl>
2012-04-17 10:58:43 +0100

    The main starting class.

--- src/org/Zeitline/Start.java
+++ src/org/Zeitline/Start.java
@@ -6,27 +6,23 @@ import org.Zeitline.Plugin.Input.InputFilter;
 import org.Zeitline.Plugin.Input.InputPluginLoader;
 import org.Zeitline.Plugin.PluginLoader;
 
+import static javax.swing.SwingUtilities.invokeLater;
+
 public final class Start {
     public static void main(String[] args) {
         // Schedule a job for the event-dispatching thread:
         // creating and showing this application's GUI.
-//        invokeLater(new Runnable() {
-//            public void run() {
-//                PluginLoader<InputFilter> pluginLoader = new InputPluginLoader("filters");
-//                IconRepository iconRepository = new IconRepository();
-//                Zeitline zeitline = new Zeitline(pluginLoader.getPlugins(), iconRepository);
-//
-//                zeitline.createAndShowGUI();
-//            }
-//        });
+        invokeLater(new Runnable() {
+            public void run() {
+                final String inputFiltersDir = "InputFilters";
 
-        final String inputFiltersDir = "InputFilters";
-        
-        FiltersProvider openFileFilters = new FiltersProvider();
-        PluginLoader<InputFilter> pluginLoader = new InputPluginLoader(inputFiltersDir);
-        IconRepository iconRepository = new IconRepository();
-        Zeitline zeitline = new Zeitline(openFileFilters.getFilters(),  pluginLoader.getPlugins(), iconRepository);
+                FiltersProvider openFileFilters = new FiltersProvider();
+                PluginLoader<InputFilter> pluginLoader = new InputPluginLoader(inputFiltersDir);
+                IconRepository iconRepository = new IconRepository();
+                Zeitline zeitline = new Zeitline(openFileFilters.getFilters(),  pluginLoader.getPlugins(), iconRepository);
 
-        zeitline.createAndShowGUI();
+                zeitline.createAndShowGUI();
+            }
+        });
     }
 }
